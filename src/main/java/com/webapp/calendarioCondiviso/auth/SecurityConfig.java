26796ab18502cab806c945f4cca716c5b45dda15
Domenico.Guagliardi <domenico.guagliardi@gmail.com>
2026-07-31 17:17:55 +0200

    Aggiunta configurazione CORS per Angular

--- src/main/java/com/webapp/calendarioCondiviso/auth/SecurityConfig.java
+++ src/main/java/com/webapp/calendarioCondiviso/auth/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.webapp.calendarioCondiviso.auth;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +13,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.webapp.calendarioCondiviso.auth.jwt.JwtFilter;
 
@@ -30,17 +35,32 @@ public class SecurityConfig {
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf(AbstractHttpConfigurer::disable)
-			.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
-			.authorizeHttpRequests(auth -> auth
-					.requestMatchers("/auth/**", "/organizzazioni/registrazione", "/utenti/registrazione/**").permitAll()
-					.requestMatchers("/organizzazioni/**","/utenti/**","/eventi/**","/inviti/**").authenticated()
-					.anyRequest()
-					.authenticated())
-			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		
+		http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+			.csrf(AbstractHttpConfigurer::disable)
+				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/auth/**", "/organizzazioni/registrazione", "/utenti/registrazione/**").permitAll()
+						.requestMatchers("/organizzazioni/**","/utenti/**","/eventi/**","/inviti/**").authenticated()
+						.anyRequest()
+						.authenticated())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		
 		return http.build();
 		
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+	    CorsConfiguration configuration = new CorsConfiguration();
+	    configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+	    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+	    configuration.setAllowedHeaders(List.of("*"));
+	    configuration.setExposedHeaders(List.of("Authorization", "X-Refresh-Token"));
+	    
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", configuration);
+	    return source;
 	}
 
 }
